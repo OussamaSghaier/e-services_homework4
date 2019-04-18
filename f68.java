c34ce046b89beedc7decb9eb8277cbc69a7129c0
OussamaSghaier <oussamasghaier1995@gmail.com>
2019-04-18 05:03:32 -0300

    effectiveness
    improving understandability
    more flexible

--- f68.java
+++ f68.java
@@ -1,2 +1,20 @@
+package oussama;
+
 public class f68{
+
+  public static String ="test test";
+  public void increment(int a)
+  {
+    a+=1;
+  }
+  
+  public void decrement(int a)
+  {
+    a+=1;
+  }
+  
+  public static void main(String[] args)
+  {
+    new f68().increment(3);
+  }
 }
